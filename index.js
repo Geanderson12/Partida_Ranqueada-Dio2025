fd35bbbff3d85d228e9943afcb7762bd059bf0a2
GEanderson12 <geanderson12@gmail.com>
2025-08-24 02:53:39 -0300

    alteração do nome da const de cálculo e um comentário

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function calculateLevel(victories, defeats) {
-  const saldovictories = victories - defeats;
+  const winningBalance = victories - defeats;
   let nivel;
 
-  if (saldovictories <= 10) {
+  if (winningBalance <= 10) {
     nivel = "Ferro";
-  } else if (saldovictories >= 11 && saldovictories <= 20) {
+  } else if (winningBalance >= 11 && winningBalance <= 20) {
     nivel = "Bronze";
-  } else if (saldovictories >= 21 && saldovictories <= 50) {
+  } else if (winningBalance >= 21 && winningBalance <= 50) {
     nivel = "Prata";
-  } else if (saldovictories >= 51 && saldovictories <= 80) {
+  } else if (winningBalance >= 51 && winningBalance <= 80) {
     nivel = "Ouro";
-  } else if (saldovictories >= 81 && saldovictories <= 90) {
+  } else if (winningBalance >= 81 && winningBalance <= 90) {
     nivel = "Diamante";
-  } else if (saldovictories >= 91 && saldovictories <= 100) {
+  } else if (winningBalance >= 91 && winningBalance <= 100) {
     nivel = "Lendário";
   } else {
     nivel = "Imortal";
   }
 
-  return `O Herói tem de saldo de ${saldovictories} está no nível de ${nivel}`;
+  return `O Herói tem de saldo de ${winningBalance} está no nível de ${nivel}`;
 }
 
+//exemplo de uso
 console.log(calculateLevel(90, 5));
